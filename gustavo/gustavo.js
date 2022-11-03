854ed24e2f4a4bc0536c62d391a19843085cd51b
Apenasumcara <vscamerano@gmail.com>
2022-11-03 13:24:38 -0300

    muda divisor

--- gustavo/gustavo.js
+++ gustavo/gustavo.js
@@ -25,7 +25,7 @@ function setup()
 function draw()
 {
   background(255);
-  image(BGImage, displayWidth/2 - mouseX/8 - rotationY/8, displayHeight/2 - mouseY/8 - rotationY/8);
+  image(BGImage, displayWidth/2 - mouseX/8 - rotationY/2, displayHeight/2 - mouseY/8 - rotationY/2);
   noStroke();
   fill(255);
   //mask
